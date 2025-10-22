82fcc3b08f93e13b49622d1439d8ace0107f2628
n7-a1 <3146638569@qq.com>
2025-10-23 03:54:11 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/treasure-game/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   build: {
     outDir: 'dist'
   }
